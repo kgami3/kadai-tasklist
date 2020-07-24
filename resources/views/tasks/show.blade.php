346271f0141edb6940751b39b6c7779385078ba9
sachi.kkk <k3.git393@gmail.com>
2020-07-24 09:30:55 +0000

    add column

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -15,6 +15,10 @@
         <th>タスク</th>
         <td>{{ $task->content }}</td>
     </tr>
+    <tr>
+        <th>ステータス</th>
+        <td>{{ $task->status }}</td>
+    </tr>
 </table>
 
 {{-- メッセージ編集ページへのリンク --}}
